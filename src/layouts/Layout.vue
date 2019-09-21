89c61a612d2a7bd27e681959bd58243a209a1c17
Jordan <jordanashment@gmail.com>
2019-09-21 08:14:38 -0600

    Initial Commit

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated>
+      <q-toolbar>
+
+        <q-toolbar-title class="absolute-center">Todo App</q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-footer>
+      <q-tabs>
+        <q-route-tab v-for="nav in navs" :key="nav.label" :icon="nav.icon" :label="nav.label" :to="nav.to" />
+
+      </q-tabs>
+    </q-footer>
+
+    <q-drawer :width=250 breakpoint="767" v-model="leftDrawerOpen" show-if-above bordered content-class="bg-primary">
+      <q-list dark>
+        <q-item-label header>Navigation</q-item-label>
+        <q-item class="text-grey-4" v-for="nav in navs" :key="nav.label" exact clickable :to="nav.to">
+          <q-item-section avatar>
+            <q-icon :name="nav.icon" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{nav.label}}</q-item-label>
+          </q-item-section>
+        </q-item>
+
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from "quasar";
+
+export default {
+  name: "MyLayout",
+  data() {
+    return {
+      leftDrawerOpen: false,
+      navs: [
+          {
+          label: 'Todo',
+          icon: 'list',
+          to: '/',
+          },
+          {
+          label: 'Settings',
+          icon: 'settings',
+          to: '/settings',
+          }
+      ],
+    };
+  },
+  methods: {
+    openURL
+  }
+};
+</script>
+
+<style lang="scss">
+  @media screen and (min-width: 768px) {
+    .q-footer{
+      display: none;
+    }
+  }
+
+.q-drawer {
+  .q-router-link--exact-active {
+    color: white !important;
+  }
+}
+</style>
---
+++ src/pages/PageTodo.vue
@@ -0,0 +1,71 @@
+<template>
+  <q-page class="q-pa-md">
+    <p>Todo Page</p>
+    <q-list separator bordered>
+      <q-item
+        clickable
+        :class="!task.completed ? 'bg-yellow-2' : 'bg-grey-2'"
+        @click="task.completed = !task.completed"
+        v-for="task in tasks"
+        :key="task.id"
+        v-ripple
+      >
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label :class="{'text-strikethrough' : task.completed}">{{task.name}}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <div class="row">
+            <div class="column justify-center">
+          <q-icon name="event" size="1.25rem" class="q-mr-xs" />
+          </div>
+          <div class="column">
+          <q-item-label class="row justify-end" caption>{{task.dueDate}}</q-item-label>
+          <q-item-label class="row justify-end" caption><small>{{task.dueTime}}</small></q-item-label>
+          </div>
+          </div>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: "Go to shop",
+          completed: false,
+          dueDate: "10/27/2019",
+          dueTime: "18:30"
+        },
+        {
+          id: 2,
+          name: "Get Bananas",
+          completed: false,
+          dueDate: "10/26/2019",
+          dueTime: "17:30"
+        },
+        {
+          id: 3,
+          name: "Get apples",
+          completed: false,
+          dueDate: "10/25/2019",
+          dueTime: "16:30"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
+
